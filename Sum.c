10359527d5a1c4ee2f20829ea126b9a06ae2f89a
Utkarsh Patel <utkarshp04@gmail.com>
2022-06-22 11:55:46 +0530

    Program to print sum of numbers input by the user

---
+++ Sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {    
+    int a,b;
+    int sum = 0;
+    printf("Enter A and B: ");
+    scanf("%d %d",&a,&b);
+    if (a<b){ 
+        for (int i = a; i<=b; i++){
+            sum+=i;
+        }
+        printf("%d",sum);
+    }
+
+    else {
+        printf("A must be greater than B");
+    }
+    
+
+}
